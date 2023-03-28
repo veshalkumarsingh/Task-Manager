6651454ba53f100e08536053d0ef0dbe14172e68
Veshal kumar singh <veshalkumar25@gmail.com>
2023-03-28 19:44:02 +0530

    Add files via upload

---
+++ Task-Manager/src/main/java/com/veshal/springboot/taskmanager/mainTaskmanager.java
@@ -0,0 +1,13 @@
+package com.veshal.springboot.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mainTaskmanager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(mainTaskmanager.class, args);
+	}
+
+}
---
+++ Task-Manager/target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'veshal', 'Python', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'veshal', 'Java', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'veshal', 'C++', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'veshal', 'Javascript', CURRENT_DATE(), false);
